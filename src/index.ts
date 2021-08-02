bc2995866942d34d7c59e2c1768b2e07e346c597
kingsley2036 <kingsley2036@qq.com>
2021-08-02 17:39:07 +0800

    使用sion测试

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 class promise{
+  succeed=null
+  fail=null
+  resolve(){
+    setTimeout(()=>{
+      this.succeed()
+     },0)
+  }
+  reject(){
+    setTimeout(()=>{
+      this.fail()
+     },0)
+  }
  constructor(fn){
    if(typeof fn !=='function'){
       throw new  Error('必须是函数')
-   }
+   } 
+   fn(this.resolve.bind(this),this.reject.bind(this))
  } 
+ then(succeed,fail){
+  this.succeed=succeed
+  this.fail=fail
+ }
 }
 export default promise
--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,50 @@
 import * as chai from 'chai'
-const assert=chai.assert
+import * as sinon from 'sinon'
+import * as  sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+const assert = chai.assert
 import promise from '../src/index'
-describe('test',()=>{
-  it('它是一个类',()=>{
-    assert.isFunction(promise)
-    assert.isObject(promise.prototype)
-  })
-  it('它只接受函数',()=>{
-    assert.throw(()=>{
-      new promise(1)
-    })
-    assert.throw(()=>{
-      new promise('1')
-    })
-    
-  })
+describe('test', () => {
+	it('它是一个类', () => {
+		assert.isFunction(promise)
+		assert.isObject(promise.prototype)
+	})
+	it('它只接受函数', () => {
+		assert.throw(() => {
+			new promise(1)
+		})
+		assert.throw(() => {
+			new promise('1')
+		})
+	})
+	it('它有一个then方法', () => {
+		const newPromise = new promise(() => {})
+		// @ts-ignore
+		assert.isFunction(newPromise.then)
+	})
+	it('new promise(fn)中的fn立即执行', () => {
+		let fn=sinon.fake()
+		const newPromise = new promise(fn)
+		// @ts-ignore
+		assert(fn.called)
+	})
+  it('new promise(fn)中的fn接受resolve和reject两个函数', () => {
+		const newPromise = new promise((resolve,reject) => {
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+		})
+
+	})
+  it('new promise(fn).then(success)中的success会在resolve被调用的时候执行', () => {
+    let success=sinon.fake()
+		const newPromise = new promise((resolve) => {
+          assert.isFalse(success.called)
+          resolve()
+          setTimeout(()=>{
+            assert(success.called)
+          })
+		})
+    // @ts-ignore
+    newPromise.then(success)
+	})
 })
